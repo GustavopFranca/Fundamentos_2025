4ec4f49a54c840f01a6eece7f96b8b95b2f99565
GustavopFranca <gustavo.p.franc@gmail.com>
2025-02-19 20:20:50 -0300

    aula2 e exercicios

---
+++ aula2/aula2.py
@@ -0,0 +1,9 @@
+#Calculadora de idade
+
+anoAtual = int(input("Informe o ano atual: "))
+anoNasc = int(input("Informe o seu ano de nascimento: "))
+idade = anoAtual - anoNasc
+print("Sua idade é:", idade)
+
+
+
---
+++ aula2/exercicio1.py
@@ -0,0 +1,5 @@
+#Calculadora e peso
+
+altura = float(input("Digite sua altura em metros: "))
+pesoIdeal = (72.7 * altura) - 58
+print(f"Seu peso ideal é: {pesoIdeal:.2f} kg")
