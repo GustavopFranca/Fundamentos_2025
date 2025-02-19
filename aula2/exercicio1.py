829e8405246ced22f8153b53aa20af93a66bf8a3
GustavopFranca <gustavo.p.franc@gmail.com>
2025-02-19 20:38:11 -0300

    Update exercicio1.py

--- aula2/exercicio1.py
+++ aula2/exercicio1.py
@@ -1,5 +1,5 @@
 #Calculadora e peso
 
-altura = float(input("Digite sua altura em metros: "))
+altura = float(input("Digite sua altura: "))
 pesoIdeal = (72.7 * altura) - 58
 print(f"Seu peso ideal é: {pesoIdeal:.2f} kg")
